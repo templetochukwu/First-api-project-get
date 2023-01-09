755b9c5daf8d0f55108751b3828d1b9f299fea55
templetochukwu <templetochukwu99@gmail.com>
2023-01-09 12:46:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Axios from "axios";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [dog, setDog] = useState("");
+
+  const getDog = () => {
+    Axios.get("https://dog.ceo/api/breeds/image/random").then((response) => {
+      setDog(response.data?.message);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Hellow Temple!
+        <span>
+          <button onClick={getDog}>Change dog</button>
+        </span>
+      </h1>
+      {dog && <img src={dog} alt="dog" />}
     </div>
   );
 }
